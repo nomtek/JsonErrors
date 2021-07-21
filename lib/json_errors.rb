e5403919ed95008de9a9932ddcf8b9faecdd29ce
Łukasz Pająk <l.pajak@nomtek.com>
2021-07-21 08:46:37 +0200

    Initial commit

---
+++ lib/json_errors.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "json_errors/version"
+
+module JsonErrors
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/json_errors_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe JsonErrors do
+  it "has a version number" do
+    expect(JsonErrors::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
