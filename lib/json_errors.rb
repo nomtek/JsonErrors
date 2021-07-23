67a16158af3b715020bc8d2b6b3fa98b898ecc0f
Łukasz Pająk <l.pajak@nomtek.com>
2021-07-23 15:51:15 +0200

    Missing requires added

--- lib/json_errors.rb
+++ lib/json_errors.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 require_relative 'json_errors/version'
+require_relative 'json_errors/config'
+require_relative 'json_errors/application_error'
+require_relative 'json_errors/rescuer'
 
 # Main module
 module JsonErrors
