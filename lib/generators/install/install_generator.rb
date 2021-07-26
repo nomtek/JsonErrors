1eb3d3fb0e91e71d88846c1e5102ff80507fd9e2
Łukasz Pająk <l.pajak@nomtek.com>
2021-07-26 08:13:06 +0200

    Generator moved

--- lib/generators/install/install_generator.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module JsonErrors
-  class InstallGenerator < Rails::Generators::NamedBase
-    source_root File.expand_path('templates', __dir__)
-  end
-end
---
+++ lib/json_errors/engine.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require 'rails'
+
+module JsonErrors
+  # Main concern module to be included to the controller
+  class Engine < ::Rails::Engine
+  end
+end
