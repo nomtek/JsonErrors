f501d32d09f7fd88e719b4f7b9734ef3459d1a19
Łukasz Pająk <l.pajak@nomtek.com>
2021-07-27 12:47:36 +0200

    Config spec added

---
+++ spec/json_errors/config_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'json_errors/config'
+
+RSpec.describe JsonErrors::Config do
+  subject { described_class.instance }
+
+  it { is_expected.to respond_to(:error_dictionary) }
+  it { is_expected.to respond_to(:custom_codes) }
+  it { is_expected.to respond_to(:error_dictionary=) }
+  it { is_expected.to respond_to(:custom_codes=) }
+
+  describe 'initializer' do
+    it 'has private initializer' do
+      expect { described_class.new }.to raise_error(NoMethodError, /private method `new' called/)
+    end
+  end
+end
